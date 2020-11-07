e56b7d88b30079757f591a1e20c698347ec14bb8
Darwin Jones <darwinj@ihme.washington.edu>
2020-11-07 15:37:38 -0800

    started hw4

---
+++ homeworks/homework4.R
@@ -0,0 +1,11 @@
+# Darwin Jones
+# HMS 520
+# 11-07-2020
+# Homework 4
+
+# 1: create a summarise_group_data function which returns a
+# dataframe with group_id columns and summary statistics
+
+summarise_group_data <- function(data, group_id, obs, fun, ...) {
+  
+}
